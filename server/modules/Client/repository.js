4e18df19e6582a9eb12874ca2fa7a132cba94de6
Pedro Augusto de Barros <pedro.barros@programmers.com.br>
2016-12-16 11:40:08 -0200

    forth commit

--- server/modules/Client/repository.js
+++
@@ -1,3 +0,0 @@
-const mongoose = require('mongoose');
-const clientSchema = require('./schema');
-export default mongoose.model('Client', clientSchema);
--- server/modules/User/repository.js
+++
@@ -1,3 +0,0 @@
-const mongoose = require('mongoose');
-const userSchema = require('./schema');
-export default mongoose.model('User', userSchema);
